96f37baeb55792e7e3a324afd05c7e78684cc122
Khurram Shehzad <khurram.pak522@gmail.com>
2017-08-08 22:37:35 +0500

    Fix detected rects not matching with actual text region when run on large screen devices

---
+++ Vision Text Detection Demo/PreviewView.swift
@@ -0,0 +1,34 @@
+//
+//  PreviewView.swift
+//  Vision Text Detection Demo
+//
+//  Created by Khurram Shehzad on 08/08/2017.
+//  Copyright © 2017 Hanjie Liu. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+class PreviewView: UIView {
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        guard let layer = layer as? AVCaptureVideoPreviewLayer else {
+            fatalError("Expected `AVCaptureVideoPreviewLayer` type for layer. Check PreviewView.layerClass implementation.")
+        }
+        
+        return layer
+    }
+    
+    var session: AVCaptureSession? {
+        get {
+            return videoPreviewLayer.session
+        }
+        set {
+            videoPreviewLayer.session = newValue
+        }
+    }
+    
+    // MARK: UIView
+    
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+}
